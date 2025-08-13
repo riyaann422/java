fa193480bdfe379a1f8518ecd3163452bdab7174
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2025-08-13 10:42:57 +0530

    Add files via upload

---
+++ odd_numbers.java
@@ -0,0 +1,14 @@
+package java_project;
+
+public class odd_numbers {
+public static void main (String []args) {
+	for(int i=0;i<0;i++) {
+		if(i%2==0) {
+			continue;
+		}
+		else {
+			System.out.println(i+"");
+		}
+	}
+}
+}
