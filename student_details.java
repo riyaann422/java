e4809d423f8a8a098d488fad130fea4e80f12548
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2025-08-13 10:43:16 +0530

    Add files via upload

---
+++ student_details.java
@@ -0,0 +1,17 @@
+package java_project;
+import java.util.Scanner;
+
+public class student_details {
+	int rollnumber;
+	String name;
+	public void getStudent details{
+		System.out.println("roll number");
+		System.out.println("name");
+		
+	pubic class main{
+		public static void main(String)
+	}
+	}
+	
+
+}
